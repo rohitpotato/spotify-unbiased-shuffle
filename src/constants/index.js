d0367b3c4c24a8571ed586f68c6c3c764b8b4079
Rohit <rohit.mmm1996@gmail.com>
2021-08-10 17:27:14 +0530

    Added Prod Url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
 const clientId = "2d0b7822774843d295a29aa196459737";
-const redirectUrl = "http://localhost:3000/callback";
+const LOCAL_URL = "http://localhost:3000/callback";
+const PROD_URL = "https://spotify-unbiased-shuffle.vercel.app/callback";
+const redirectUrl =
+  process.env.NODE_ENV === "production" ? PROD_URL : LOCAL_URL;
 const scopes = [
   "playlist-read-private",
   "playlist-read-collaborative",
