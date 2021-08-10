7d6fc5bd6e2d1e00ac6efb989e0e3c2c3e9d028a
Rohit <rohit.mmm1996@gmail.com>
2021-08-11 01:34:38 +0530

    Added Exclusion for local tracks

--- src/utils/common.js
+++ src/utils/common.js
@@ -58,9 +58,11 @@ export const hashParams = () => {
 export const shuffleArray = (arr) => {
   const array = [...arr];
   for (let i = array.length - 1; i > 0; i -= 1) {
-    const j = Math.floor(Math.random() * (i + 1));
-    // eslint-disable-next-line no-param-reassign
-    [array[i], array[j]] = [array[j], array[i]];
+    if (!arr[i]?.includes("local")) {
+      const j = Math.floor(Math.random() * (i + 1));
+      // eslint-disable-next-line no-param-reassign
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   }
   return array;
 };
